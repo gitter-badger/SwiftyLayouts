cc01771c28af9a4d154aa25064391f34a5c61a99
kaushal <kaushal@Abhisheks-Mac-mini.local>
2018-02-05 17:08:27 +0530

    added development pod

--- SwiftyLayoutsExample/Pods/Target Support Files/Pods-SwiftyLayoutsExample/Pods-SwiftyLayoutsExample-frameworks.sh
+++ SwiftyLayoutsExample/Pods/Target Support Files/Pods-SwiftyLayoutsExample/Pods-SwiftyLayoutsExample-frameworks.sh
@@ -100,6 +100,10 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyLayouts/SwiftyLayouts.framework"
+fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
